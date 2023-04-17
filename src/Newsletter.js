57eda667ba39ccf566679d1daea36208f39a1b46
Mckeydown <mckeydown@outlook.com>
2023-04-17 16:56:34 +0300

    newsletter page edit

--- src/Newsletter.js
+++ src/Newsletter.js
@@ -10,8 +10,6 @@ export default function Newsletter() {
 
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredCollections, setFilteredCollections] = useState([]);
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
 
 
   const handleSearch = (event) => {
@@ -26,30 +24,13 @@ export default function Newsletter() {
     );
   }, [searchTerm]);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (isLoading || filteredCollections.length <= page * 8) return;
-      if (
-        window.innerHeight + document.documentElement.scrollTop !==
-        document.documentElement.offsetHeight
-      )
-        return;
-        setIsLoading(true);
-        setTimeout(() => {
-          setPage((prevPage) => prevPage + 1);
-          setIsLoading(false);
-        }, 1000);
-    };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [isLoading, filteredCollections, page]);
     
   return (
       <div className='newsletter-container'>
           <h1>SELECT A COLLECTION TO SEE ANNOUNCEMENTS</h1>
           <input placeholder='SEARCH' type="text" value={searchTerm} onChange={handleSearch} />
           <div className='collections-container'>
-          {filteredCollections.slice(0, page * 8).map((collection) => (
+          {filteredCollections.map((collection) => (
           <CollectionItem
             key={collection.name}
             collectionName={collection.name}
@@ -58,7 +39,6 @@ export default function Newsletter() {
           />
         ))}
           </div>
-          {isLoading && <p id='loading-text'>Loading...</p>}
       </div>
   )
 }
